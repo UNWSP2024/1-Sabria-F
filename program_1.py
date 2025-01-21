a25663a7e2a2ac3a6b71b0e1f0692427d1f3c00b
Sabria-F <sjfafach@students.unwsp.edu>
2025-01-21 11:19:50 -0600

    SabriaFafachprogram_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,5 @@
-def hello_world():
-    # Update the message below to say "Hello World", 
-    # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+#By: Sabria Fafach
+#Date: January 15, 2025
+#Name: program_1.py
 
-hello_world()
+print('Hello World')
